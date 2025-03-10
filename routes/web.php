d853240200dc546e802bacc2707b69beb5fbe371
Bayu Rizky Kurnia Pratama <bayurkp.dev@gmail.com>
2025-03-11 07:34:02 +0800

    feat: add map route

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,15 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get('/map', function () {
+    return Inertia::render('map');
+})->name('map');
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
